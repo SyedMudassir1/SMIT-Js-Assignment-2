25fed4aa9d855598852a2a362ef1d3a43ea3ef39
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-01-31 05:54:49 -0800

    Add files via upload

---
+++ Assigment 2,2/app.js
@@ -0,0 +1,25 @@
+//*********** Assignment # 2 *************//
+
+//Question 1 
+//var age = "i am 19 year old";
+//alert("age");
+
+//Question 2 
+// var web pages = "you have visited this site 14 times";
+// alert("web pages");
+
+//Question 3 
+// var birth year = "my birth year is 2003";
+// alert("birth year");
+// document.write("my birth year is 2003");
+
+//Question 4 (a)
+// var name = "Ali";
+// alert("name");
+
+//Question 4 (b)
+// var product title = "T shirt";
+// alert("product title");
+
+//Question 4 (c)
+//document.write("Ali orderd 5 T shirts on XYZ clothing store");
