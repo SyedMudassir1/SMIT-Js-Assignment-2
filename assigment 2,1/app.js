b2cb9fc9a051d5965722aa2b1f7fbdfff435ea2c
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-01-31 05:54:14 -0800

    Add files via upload

---
+++ assigment 2,1/app.js
@@ -0,0 +1,46 @@
+                  //***** chp # 2 *****
+//Question 1
+//var username = "Ali567";
+// alert("username");
+
+//Question 2
+// var MyName = "Ali";
+
+//Question 3 (a)
+// var messsage = "hi"
+
+//Question 3 (b)
+//var message = "Hello World"
+
+//Question 3 (c)
+// alert("message");
+
+//Question 4
+// var name = "Ali";
+// alert("name");
+
+// var age = "19 year old"
+//alert("age");
+
+// var profession = "Student"
+// alert("profession");
+
+// Question 5
+// alert("pizza /npizz /npiz /npi /np");
+
+//question 6 
+// var email = "ali@gmail.com";
+// alert("email");
+
+//Question 7 
+// var = "i am tring to learn from the book a smarter /n way to learn javascript"
+// alert("a smarter way to learn to jawascript");
+
+//Question 8 
+//document.write("yah! i can write html content through jawascript");
+
+//Question 9
+//document.write("======%%%%%$$$%%%%=======");
+// alert("====%%%$$$%%%===");
+
+//                       =====END=====
