3c124b5e55d01ed1041b24d9c82b6713cb020e39
SyedMudassir1 <122387591+SyedMudassir1@users.noreply.github.com>
2023-01-31 05:55:16 -0800

    Add files via upload

---
+++ Assignment 2,3/app.js
@@ -0,0 +1,19 @@
+ //************* Assignment # 2  ***************//
+ //Question 1 
+ //document.write("<h1>Rules For Naming JS Variables<h1>");
+
+ //Question 1 (b)
+ // document.write("variables names can only contain letters, numbers,dolllar signs, and undersscores.");
+  
+ //Question 1 (c)
+ // document.write("veriable must begin with a letter,$ and underscores.");
+
+ //Question 1 (d)
+ // document.write("varible names are case sensitive")
+
+ //Question 1 (e)
+ // document.write("variable names should not be js kerword")
+
+//                     ============END===============
+
+
